57ee7b7f635badc019aff6a95c5a2b51977a15ae
Elio Dredd <143877390+Ellio-Dredd@users.noreply.github.com>
2024-11-20 00:57:55 +0530

    Master

--- app/src/main/java/com/example/rainy/Details.java
+++ app/src/main/java/com/example/rainy/Details.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -19,10 +21,19 @@ public class Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_details);
+
+        RecyclerView recyclerView = findViewById(R.id.fRecylerVeiw);
+
         setUpdayForcastModels();
+
+        df_ReceyclerViewAdapter adapter = new df_ReceyclerViewAdapter(this,dayForcastModels);
+        //make sure to make this after Setting up the forcast models
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
