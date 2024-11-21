a620455a6fa9dc663fa38469ee924daef166d375
Elio Dredd <143877390+Ellio-Dredd@users.noreply.github.com>
2024-11-22 00:46:14 +0530

    Master

--- app/src/main/java/com/example/rainy/Details.java
+++ app/src/main/java/com/example/rainy/Details.java
@@ -1,8 +1,12 @@
 package com.example.rainy;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Details extends AppCompatActivity {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     ArrayList<dayForcastModel> dayForcastModels = new ArrayList<>() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firbase();
 
 
         EdgeToEdge.enable(this);
@@ -27,6 +41,7 @@ public class Details extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.fRecylerVeiw);
 
         setUpdayForcastModels();
+
 
         df_ReceyclerViewAdapter adapter = new df_ReceyclerViewAdapter(this,dayForcastModels);
         //make sure to make this after Setting up the forcast models
@@ -54,4 +69,33 @@ public class Details extends AppCompatActivity {
 
 
     }
+
+
+// for database uses ---------------------------------------------------------
+
+   public void firbase() {
+        Map<String, Object> city = new HashMap<>();
+        city.put("name", "Los Angeles");
+        city.put("state", "CA");
+        city.put("country", "USA");
+
+       db.collection("cities").document("LA")
+               .set(city)
+               .addOnSuccessListener(new OnSuccessListener<Void>() {
+                   @Override
+                   public void onSuccess(Void aVoid) {
+                       Log.d(TAG, "DocumentSnapshot successfully written!");
+                   }
+               })
+               .addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                       Log.w(TAG, "Error writing document", e);
+                   }
+               });
+
+
+
+
+   }
 }
